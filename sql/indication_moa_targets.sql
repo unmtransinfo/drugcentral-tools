d4d97e12b565ee003fa1434fb6a42c57a03a411a
Jeremy Yang <jeremyjyang@gmail.com>
2020-07-08 15:52:33 -0600

    Ok.

--- sql/indication_moa_targets.sql
+++ sql/indication_moa_targets.sql
@@ -1,11 +1,10 @@
-SELECT
-        atf.act_id,
+SELECT DISTINCT
+        omop.concept_name AS omop_concept,
+        omop.umls_cui,
         atf.struct_id,
         s.name AS struct_name,
         atf.target_id,
         atf.target_name,
-        atf.target_class,
-        atf.accession,
         atf.gene,
         atf.action_type,
         atf.moa_source,
@@ -14,12 +13,13 @@ SELECT
         r.pmid,
         r.doi,
         r.title,
-        r.authors,
         r.dp_year
 FROM
         act_table_full atf
 JOIN structures s ON s.id = atf.struct_id
 JOIN reference r ON r.id = atf.moa_ref_id
+JOIN omop_relationship omop ON omop.struct_id = s.id
 WHERE
         moa > 0
+        AND omop.relationship_name = 'indication'
 ;
