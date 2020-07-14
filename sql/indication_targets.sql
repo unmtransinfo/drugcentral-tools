3c971999336063a5e9693dab133b9e135c54a659
Jeremy Yang <jeremyjyang@gmail.com>
2020-07-14 15:01:05 -0600

    Ok.

--- sql/indication_targets.sql
+++ sql/indication_targets.sql
@@ -7,6 +7,11 @@ SELECT DISTINCT
         atf.target_name,
         atf.gene,
         atf.action_type,
+        atf.act_source,
+        atf.act_type,
+        atf.act_comment,
+        atf.relation,
+        atf.moa,
         atf.moa_source,
         atf.moa_source_url,
         atf.moa_ref_id,
@@ -17,9 +22,10 @@ SELECT DISTINCT
 FROM
         act_table_full atf
 JOIN structures s ON s.id = atf.struct_id
-JOIN reference r ON r.id = atf.moa_ref_id
 JOIN omop_relationship omop ON omop.struct_id = s.id
+LEFT OUTER JOIN reference r ON r.id = atf.moa_ref_id
 WHERE
-        moa > 0
-        AND omop.relationship_name = 'indication'
+        --atf.moa > 0
+        --AND 
+        omop.relationship_name = 'indication'
 ;
