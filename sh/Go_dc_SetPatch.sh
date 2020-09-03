0672af0507ef4a4e40a4b0e7c85b6e4c78237ca3
Jeremy Yang <jeremyjyang@gmail.com>
2020-09-03 15:36:37 -0600

    Ok.

--- sh/Go_dc_SetPatch.sh
+++ sh/Go_dc_SetPatch.sh
@@ -6,7 +6,11 @@ DBNAME="drugcentral"
 SCHEMA="public"
 #
 ###
+function strstrip {
+  echo "$1" |sed -e 's/^\s*//' |sed -e 's/\s*$//'
+}
 #
+psql -d $DBNAME -c "ALTER TABLE structures DROP COLUMN status"
 psql -d $DBNAME -c "ALTER TABLE structures ADD COLUMN status varchar(10)"
 ###
 # Set	DrugCentral_ID	Name
@@ -15,6 +19,8 @@ ifile="$SRCDATADIR/$ifile_name"
 N=$(($(cat $ifile |grep -v '^\s*$' |wc -l) - 1))
 printf "Loading %s: %d rows\n" "$ifile" "$N"
 i=0
+n_err=0
+n_val=0
 while [ "$i" -lt "$N" ]; do
 	i=$(($i + 1))
 	ii=$(($i + 1))
@@ -22,10 +28,19 @@ while [ "$i" -lt "$N" ]; do
 	s=$(echo "$line" |awk -F '\t' '{print $1}'|sed -e 's/\s//g')
 	struct_id=$(echo "$line" |awk -F '\t' '{print $2}')
 	name=$(echo "$line" |awk -F '\t' '{print $3}')
-	printf "%d. struct_id=\"%s\"; name=\"%s\"; set=\"%s\"\n" "$i" "$struct_id" "$name" "$s"
+	dc_name="$(strstrip "$(psql -d $DBNAME -tc "SELECT name FROM structures WHERE id = $struct_id")")"
+	printf "%d. struct_id=\"%s\"; set=\"%s\"; name=\"%s\"; dc_name=\"%s\"\n" "$i" "$struct_id" "$s" "$name" "$dc_name"
 	#
-#	if [ "$struct_id" -a "$s" ]; then
-#		psql -q -d $DBNAME -c "UPDATE structures SET status = '$s' WHERE id = $struct_id"
-#	fi
+	if [ "$name" != "$dc_name" ]; then
+		printf "%d. WARNING: name=\"%s\" != dc_name=\"%s\"\n" "$i" "$name" "$dc_name"
+		n_err=$(($n_err + 1))
+	fi
+	if [ "$struct_id" -a "$s" ]; then
+		psql -q -d $DBNAME -c "UPDATE structures SET status = '$s' WHERE id = $struct_id"
+		n_val=$(($n_val + 1))
+	fi
 done
+#
+printf "N_row: %d; n_val: %d; n_err: %d\n" "$N" "$n_val" "$n_err"
+#
 #
