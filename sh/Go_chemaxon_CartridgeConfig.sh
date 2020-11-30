9fafd5cfd6deee136965e3ce4f8011c26497a1bd
Jeremy Yang <jeremyjyang@gmail.com>
2020-11-30 10:38:13 -0700

    Ok.

---
+++ sh/Go_chemaxon_CartridgeConfig.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+###
+# https://docs.chemaxon.com/display/docs/getting-started-jchem-postgresql-cartridge.md
+# https://docs.chemaxon.com/display/docs/jchem-postgresql-cartridge-manual.md
+###
+#sudo dpkg -i jchem-psql_20.18.0.r12267_amd64.deb
+#sudo service jchem-psql init
+#sudo cp ~/.chemaxon/license.cxl /etc/chemaxon
+#sudo service jchem-psql manual-start
+#sudo -u postgres createuser chemaxon
+#sudo -u postgres createdb chemaxon -O chemaxon
+#sudo -u postgres psql -d chemaxon -c "CREATE EXTENSION chemaxon_type"
+#sudo -u postgres psql -d chemaxon -c "CREATE EXTENSION hstore"
+#sudo -u postgres psql -d chemaxon -c "CREATE EXTENSION chemaxon_framework"
+#sudo -u postgres psql -d chemaxon -c "SELECT 'C'::Molecule('sample') |<| 'CC'::Molecule"
+###
+# Modify existing drugcentral db:
+DBNAME="drugcentral"
+#
+sudo -u postgres psql -d $DBNAME -c "CREATE EXTENSION chemaxon_type"
+sudo -u postgres psql -d $DBNAME -c "CREATE EXTENSION hstore"
+sudo -u postgres psql -d $DBNAME -c "CREATE EXTENSION chemaxon_framework"
+sudo -u postgres psql -d $DBNAME -c "SELECT 'C'::Molecule('sample') |<| 'CC'::Molecule"
+psql -d $DBNAME -c "SELECT 'C'::Molecule('sample') |<| 'CC'::Molecule"
+#
+###
+sudo -u postgres psql -d $DBNAME -c "ALTER TABLE structures ADD COLUMN cx_mol MOLECULE('sample')"
+sudo -u postgres psql -d $DBNAME -c "UPDATE structures SET cx_mol = smiles::Molecule('sample')"
+#
+qsmi="NCCc1ccc(O)c(O)c1"
+psql -qAF $'\t' -d $DBNAME -c "SELECT name,smiles FROM structures WHERE 'NCCc1ccc(O)c(O)c1'::Molecule |=| cx_mol"
+psql -qAF $'\t' -d $DBNAME -c "SELECT name,smiles FROM structures WHERE 'NCCc1ccc(O)c(O)c1'::Molecule |>| cx_mol"
+psql -qAF $'\t' -d $DBNAME -c "SELECT name,smiles FROM structures WHERE 'NCCc1ccc(O)c(O)c1'::Molecule |<| cx_mol ORDER BY cd_molweight LIMIT 10"
+psql -qAF $'\t' -d $DBNAME -c "SELECT name, smiles, (cx_mol |~| '${qsmi}') AS similarity FROM structures WHERE ('${qsmi}', 0.6)::sim_filter |<~| cx_mol ORDER BY similarity DESC LIMIT 10"
+#
